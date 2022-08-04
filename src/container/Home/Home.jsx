e26d59b958d75f312f6ff787b92a6267844d95bc
ayyubnurumi <ayyubinvestment@gmail.com>
2022-08-04 17:10:01 +0700

    belajar react

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
-import YTComp from "../../component/YTComp/YTComp";
-import Product from "../Product/Product";
+// import YTComp from "../../component/YTComp/YTComp";
+import LifeCycleComp from "../LifeCycleComp/LifeCycleComp";
+// import Product from "../Product/Product";
 
 class Home extends Component {
+  state = {
+    showComponent: true
+  }
+
+  componentDidMount(){}
+
   render() {
     return (
       <div>
-        <p>youtube component</p>
+        {/* <p>youtube component</p>
         <hr/>
         <YTComp time="19.95" title="konspirasi op. eps.1001" desc="978.785 kali ditonton. 29 hari yang lalu."/>
         <YTComp time="11.17" title="konspirasi op. eps.1002" desc="876.786 kali ditonton. 22 hari yang lalu."/>
@@ -16,7 +23,16 @@ class Home extends Component {
         <hr/>
         <p>counter</p>
         <hr/>
-        <Product />
+        <Product /> */}
+        <p>lifecycle component</p>
+        <hr/>
+        {
+          this.state.showComponent 
+          ?
+          <LifeCycleComp/>
+          : null
+        }
+        
       </div>
     )
   }
