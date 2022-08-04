b200fc1522780473f7c62024ea4d97c78f12e17d
ayyubnurumi <ayyubinvestment@gmail.com>
2022-08-04 14:55:44 +0700

    belajar react

--- src/container/Product/Product.jsx
+++ src/container/Product/Product.jsx
@@ -1,37 +1,38 @@
-import React, {Component, Fragment} from "react";
-import "../Product/Product.css"
-import Troley from "../Product/Troley.jpg"
+import React, { Component, Fragment } from "react";
+import "../Product/Product.css";
+import Troley from "../Product/Troley.jpg";
+import CardProduct from "./CardProduct/CardProduct";
 
 class Product extends Component {
-    render (){
-        return(
-            <Fragment>
-                <div className="header">
-                    <div className="logo">
-                        <img src="https://www.zilliondesigns.com/blog/wp-content/uploads/001-min.png" alt="logo"
-                        height="70px"/>
-                    </div>
-                    <div className="troley">
-                        <img src={Troley} alt="troley"/>
-                        <div className="count">3</div>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="img-thumb-prod">
-                        <img src="https://jualayamhias.com/wp-content/uploads/2017/12/Cemani-chicken-e1513914416319.jpg" alt="ayam kecap"
-                        height="320px" width="320px" />
-                    </div>
-                    <p className="product-title">ayam bumbu kecap</p>
-                    <p className="product-price">Rp 500</p>
-                    <div className="counter">
-                        <button className="minus">-</button>
-                        <input type="text" value={3}/>
-                        <button className="plus">+</button>
-                    </div>
-                </div>
-            </Fragment>
-        )
-    }
+  state = {
+    order: 4,
+  };
+
+  handleCounterChange = (newValue) => {
+    this.setState({
+        order: newValue
+    })
+  }
+  render() {
+    return (
+      <Fragment>
+        <div className="header">
+          <div className="logo">
+            <img
+              src="https://www.zilliondesigns.com/blog/wp-content/uploads/001-min.png"
+              alt="logo"
+              height="70px"
+            />
+          </div>
+          <div className="troley">
+            <img src={Troley} alt="troley" />
+            <div className="count">{this.state.order}</div>
+          </div>
+        </div>
+        <CardProduct onCounterChange={(value) => this.handleCounterChange(value)} />
+      </Fragment>
+    );
+  }
 }
 
 export default Product;
