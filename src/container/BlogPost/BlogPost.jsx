643df4d8df11bf4b3fd2a0d772895a5e949a2c8f
ayyubnurumi <ayyubinvestment@gmail.com>
2022-08-23 16:29:35 +0700

    praxis

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -6,23 +6,32 @@ import "./BlogPost.css";
 class BlogPost extends Component {
   state = {
     post: [],
+    formBlogPost: {
+      id: 1,
+      title: "",
+      body: "",
+      userId: 1,
+    },
   };
 
   getPostAPI = () => {
-    axios.get("http://localhost:3004/posts")
-    .then((result) => {
-        this.setState({
-            post: result.data
-        })
-    })
-  }
+    axios.get("http://localhost:3004/posts").then((result) => {
+      this.setState({
+        post: result.data,
+      });
+    });
+  };
 
   handleRemove = (data) => {
     //console.log(data)
-    axios.delete("http://localhost:3004/posts/" + (data)).then((ress) => {
-      this.getPostAPI()
-    })
-  }
+    axios.delete("http://localhost:3004/posts/" + data).then((ress) => {
+      this.getPostAPI();
+    });
+  };
+
+  handleFormChange = () => {
+    console.log("form changed");
+  };
 
   componentDidMount() {
     // fetch("https://jsonplaceholder.typicode.com/posts")
@@ -39,6 +48,25 @@ class BlogPost extends Component {
     return (
       <Fragment>
         <p className="section-title">blog post</p>
+        <div className="form-add-post">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            name="title"
+            placeholder="add title"
+            onChange={this.handleFormChange}
+          />
+          <label htmlFor="body-content">blog content</label>
+          <textarea
+            name="body-content"
+            id="body-content"
+            cols="30"
+            rows="10"
+            placeholder="add blog content"
+            onChange={this.handleFormChange}
+          ></textarea>
+          <button className="btn-submit">save</button>
+        </div>
         {this.state.post.map((post) => {
           return <Post key={post.id} data={post} remove={this.handleRemove} />;
         })}
